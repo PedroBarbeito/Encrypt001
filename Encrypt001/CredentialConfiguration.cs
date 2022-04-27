96fcda3b7dc463d38cd8d816456b8ebdd1561564
pedro_barbeito <pedro.barbeito@gmail.com>
2022-04-27 22:42:59 +0200

    #01 commit

--- Encrypt001/CredentialConfiguration.cs
+++ Encrypt001/CredentialConfiguration.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//----------------------
 namespace Encrypt001
 {
     public class CredentialConfiguration
